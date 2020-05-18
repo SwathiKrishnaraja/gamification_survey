bbf083a6508b69d2ae9aa212b14085269e1bfe99
SwathiKrishnaraja <swathikrishnaraja149@gmail.com>
2020-05-18 21:21:54 +0200

    Adds react router to handle page routing

--- gamified_survey/src/index.tsx
+++ gamified_survey/src/index.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
+import ConsentForm from './pages/ConsentForm'
+import GamifiedSurvey from './pages/GamifiedSurvey'
+import TraditionalSurvey from './pages/TraditionalSurvey'
+import PreSurvey from './pages/PreSurvey'
 import * as serviceWorker from './serviceWorker';
 
+const Routing = () => {
+  return (
+    <Router>
+      <Route exact path='/' component={App} />
+      <Route path='/ConsentForm' component={ConsentForm} />
+      <Route path='/GamifiedSurvey' component={GamifiedSurvey} />
+      <Route path='/TraditionalSurvey' component={TraditionalSurvey} />
+      <Route path='/PreSurvey' component={PreSurvey} />
+    </Router>
+  )
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Routing />,
   document.getElementById('root')
 );
 
