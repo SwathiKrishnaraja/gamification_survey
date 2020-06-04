25367bc5afdee2dd7f081bd9b33823b61d225f63
SwathiKrishnaraja <swathikrishnaraja149@gmail.com>
2020-06-04 17:37:42 +0200

    Adds modal components and adds the survey selection page

--- gamified_survey/src/components/Footer.tsx
+++ gamified_survey/src/components/Footer.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ImprintModal from './ImprintModal';
+import ImprintContent from './ImprintContent'
 const Footer = () => {
+  const [showImprint, setShowImprint] = useState<boolean>(false)
+  const handleImprintClick = () => {
+    setShowImprint(showImprint ? false : true)
+  }
   return (
     <footer>
       <div className="home-footer">
-        <button className="imprint-button">Imprint</button>
+        <button className="imprint-button" onClick={handleImprintClick}>Imprint</button>
+        <ImprintModal showModal={showImprint} handleClick={handleImprintClick} children={<ImprintContent />} />
         <label>Copyrights reserved by Saarland University</label>
         <label>Survey published under MIT license</label>
       </div>
--- gamified_survey/src/pages/Home.tsx
+++ gamified_survey/src/pages/Home.tsx
@@ -3,7 +3,9 @@ import Footer from '../components/Footer'
 import HomeContent from '../components/HomeContent';
 import Header from '../components/Header';
 
+
 const Home = () => {
+
   return (
     <div className="container">
       <Header />
