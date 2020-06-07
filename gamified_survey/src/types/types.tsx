23e412c061199b5fa552a29fbc65e9966d1227f1
SwathiKrishnaraja <swathikrishnaraja149@gmail.com>
2020-06-07 19:32:19 +0200

    Takes the interfaces to a seperate file

---
+++ gamified_survey/src/types/types.tsx
@@ -0,0 +1,20 @@
+export interface Questions {
+    type: 'checkbox' | 'radiogroup' | 'matrix' | 'text',
+    name: string,
+    title: string,
+    columns?: Array<MatrixObject>
+    rows?: Array<MatrixObject>
+    choices?: Array<string>,
+    isRequired?: boolean,
+    hasSelectAll?: true,
+
+}
+
+export interface MatrixObject {
+    value: number | string
+    text: string
+}
+export interface Json {
+    questions: Array<Questions>
+    completeText: string
+}
