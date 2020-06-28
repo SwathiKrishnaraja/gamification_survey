3b9707f449d984c07aeb534269c059e8e6ff1b5e
SwathiKrishnaraja <swathikrishnaraja149@gmail.com>
2020-06-28 13:48:21 +0200

    adjust homepage logos

--- gamified_survey/src/components/HomeContent.tsx
+++ gamified_survey/src/components/HomeContent.tsx
@@ -1,8 +1,8 @@
 import React, { Fragment } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import udsLogo from '../content/udsLogo.png'
-import dfkiLogo from '../content/dfkiLogo.png'
+import udsLogo from '../content/sic.png'
+import dfkiLogo from '../content/DFKI_Logo.jpg'
 import umtlLogo from '../content/umtlLogo.png'
 
 const HomeContent = () => {
@@ -24,9 +24,8 @@ const HomeContent = () => {
         </p>
         <p style={{ textAlign: 'left' }}> {t('homeContent.name')}</p>
         <div className='home-page-logos'>
-          <img style={{ width: 200, height: 80 }} alt='uds' src={udsLogo} />
-          <img style={{ width: 300, height: 70 }} alt='dfki' src={dfkiLogo} />
-          <img style={{ width: 80, height: 75 }} alt='umtl' src={umtlLogo} />
+          <img alt='uds' src={udsLogo} />
+          <img alt='dfki' src={dfkiLogo} />
         </div>
         <div style={{ textAlign: 'left' }}>
           <h4>*Data Protection and Participation Information</h4>
