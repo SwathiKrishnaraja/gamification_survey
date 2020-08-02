ec5160fd8c4f3aa85f874a9e804d3f3f3c760d83
SwathiKrishnaraja <swathikrishnaraja149@gmail.com>
2020-08-02 12:46:10 +0200

    feat: provide hint to the user to agree to data protection
    * use styled component and state variable to notify user to agree to data protection

--- gamified_survey/src/components/HomeContent.tsx
+++ gamified_survey/src/components/HomeContent.tsx
@@ -5,20 +5,26 @@ import udsLogo from '../content/sic.png'
 import dfkiLogo from '../content/DFKI_Logo.jpg'
 import ImprintContent from '../components/ImprintContent'
 import ImprintModal from '../components/ImprintModal'
+import styled from 'styled-components'
 
 const HomeContent = () => {
   const { t } = useTranslation()
   const history = useHistory()
   const [showModal, setShowModal] = useState<boolean>(false)
   const [hasAgreed, setHasAgreed] = useState<boolean>(false)
+  const [isChecked, setIsChecked] = useState<boolean>(true)
 
   const handleDataProtectionTagClick = () => {
     setShowModal(showModal ? false : true)
   }
 
   const handleCheckBoxClick = () => {
-    setHasAgreed(true)
+    setHasAgreed(hasAgreed ? false : true)
   }
+
+  const PrivacyHeading = styled.h4`
+  color: ${isChecked ? "black" : "red"}
+  `
 
   return (
     <Fragment>
@@ -39,14 +45,14 @@ const HomeContent = () => {
           <img alt='dfki' src={dfkiLogo} />
         </div>
         <div style={{ textAlign: 'left' }}>
-          <h4>*Data Protection and Participation Information</h4>
+          <PrivacyHeading>*Data Protection and Participation Information</PrivacyHeading>
           <input type='checkbox' onClick={handleCheckBoxClick} />
           <label>I have read and understood the information on <a style={{ textDecoration: 'none', color: '#337ab7' }} onClick={handleDataProtectionTagClick} href='# '>data protection</a> and the participation information and agree that my data may be used anonymously for the mentioned purposes</label>
           <ImprintModal children={<ImprintContent />} handleClick={handleDataProtectionTagClick} showModal={showModal} title='Privacy and General Information' />
         </div>
 
       </div>
-      <button className="continue-button" onClick={() => hasAgreed ? history.push('/PreSurvey') : null}>{t('homeContent.button')}</button>
+      <button className="continue-button" onClick={() => hasAgreed ? history.push('/PreSurvey') : setIsChecked(false)}>{t('homeContent.button')}</button>
     </Fragment>
   );
 };
