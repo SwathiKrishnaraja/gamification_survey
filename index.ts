a5693fbd6d427e113cec85ffd09d8c050774dd27
SwathiKrishnaraja <swathikrishnaraja149@gmail.com>
2020-08-11 22:51:25 +0200

    fix: use actual react-app scripts and point server to react build

--- index.ts
+++ index.ts
@@ -4,13 +4,13 @@ import path from 'path'
 const app: express.Application = express()
 const PORT = process.env.PORT || 8080
 
-app.use(express.static(`${path.resolve('./')}/build/client`))
+app.use(express.static(`${path.resolve('./')}/client/build`))
 
 app.get('/submit', (req, res) => {
     res.send('Reached API')
 })
 app.get('*', (req, res) => {
-    res.sendFile(`${path.resolve('./')}'/build/client/index.html`)
+    res.sendFile(`${path.resolve('./')}'/client/build/index.html`)
 })
 
 
