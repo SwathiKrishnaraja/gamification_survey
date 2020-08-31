d15c0625c8a44ce829ff4575cb93dbd7abab46a1
SwathiKrishnaraja <swathikrishnaraja149@gmail.com>
2020-08-31 17:23:16 +0200

    feat: expose api from backend
    * expose apis for CRUD operations

--- index.ts
+++ index.ts
@@ -1,48 +1,75 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import path from 'path'
 import { PrismaClient } from '@prisma/client'
+import bodyParser from 'body-parser'
 
 const prisma = new PrismaClient()
 
-async function main() {
+const app: express.Application = express()
+const PORT = process.env.PORT || 8080
 
-    await prisma.participants.create({
-        data: {
-            survey_mode: 'GAMIFIED',
-            result: {
-                "q1": "Healthy",
-            },
-            badges: 8,
-        }
-    })
+app.use(express.static(`${path.resolve('./')}/client/build`))
+app.use(bodyParser.json())
 
-    const allParticipants = await prisma.participants.findMany()
-    console.log(allParticipants)
+app.post('/exit', async (req: Request, res: Response) => {
+    try {
+        const data = await req.body
+        const exitCase = await prisma.exitCandidates.create({
+            data: data
+        })
+        res.send({
+            status: 200,
+            message: 'Success'
+        })
+    } catch (e) {
 
-}
+    }
+})
 
-main()
-    .catch(e => {
-        throw e
-    })
-    .finally(async () => {
-        await prisma.$disconnect()
-    })
+app.post('/submit', async (req: Request, res: Response) => {
+    try {
+        const data = await req.body
+        const participantInfo = await prisma.participants.create({
+            data: data
+        })
+        res.send({
+            status: 200,
+            message: 'Success'
+        })
+    } catch (error) {
+        throw error
+    }
+})
+
+app.post('/mode', async (req: Request, res: Response) => {
+    try {
+        const data = await req.body
+        const mode = await prisma.mode.create({
+            data: data
+        })
+        res.send({
+            status: 200,
+            message: 'Success'
+        })
+
+    } catch (error) {
+        throw error
+    }
+})
+
+app.get('/mode', async (req, res) => {
+    try {
+        const mode = await prisma.mode.findMany()
+        res.send(mode)
+
+    } catch (error) {
+        throw (error)
+    }
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(`${path.resolve('./')}'/client/build/index.html`)
+})
 
 
-
-
-// const app: express.Application = express()
-// const PORT = process.env.PORT || 8080
-
-// app.use(express.static(`${path.resolve('./')}/client/build`))
-
-// app.get('/submit', (req, res) => {
-//     res.send('Reached API')
-// })
-// app.get('*', (req, res) => {
-//     res.sendFile(`${path.resolve('./')}'/client/build/index.html`)
-// })
-
-
-// app.listen(PORT, () => `server listening at ${PORT}`)
+app.listen(PORT, () => `server listening at ${PORT}`)
