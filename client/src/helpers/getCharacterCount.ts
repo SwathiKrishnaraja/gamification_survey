01573e8b1d038bc7c3a626dc03caf2df8417ca94
SwathiKrishnaraja <swathikrishnaraja149@gmail.com>
2020-09-05 09:29:14 +0200

    fix: add precheck for incoming list

--- client/src/helpers/getCharacterCount.ts
+++ client/src/helpers/getCharacterCount.ts
@@ -11,7 +11,7 @@ const splitString = (inputString: string) => inputString.split('')
 const getNumberOfCharacters = (listOfWords: Array<string>) => listOfWords && listOfWords.length
 
 const getCharacterCount = (listOfOpenQuestions: Array<OpenQuestions>): Array<OpenQuestions> => {
-    listOfOpenQuestions.map(question => {
+    listOfOpenQuestions && listOfOpenQuestions.map(question => {
         const data = Object.keys(question).forEach(key => {
             const listOfWords = splitString(question[key] as string)
             const wordCount = getNumberOfCharacters(listOfWords)
