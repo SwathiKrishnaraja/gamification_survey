957c8484eaba5b9bd716b1dd72ed4e5a098d0cf2
SwathiKrishnaraja <swathikrishnaraja149@gmail.com>
2020-09-27 16:09:07 +0200

    fix: adjust the fetch call with no-cors mode

--- client/src/api/getSurveyMode.ts
+++ client/src/api/getSurveyMode.ts
@@ -1,6 +1,11 @@
+
 const getSurveyMode = async () => {
-    const surveyModeResponse = await fetch('http://localhost:8080/mode')
-    const modes = await surveyModeResponse.json()
-    return modes
+    const response = await fetch('http://localhost:8080/mode', {
+        mode: 'no-cors',
+    })
+    const data = await response.json()
+    const mode = await data.body
+    await console.log(data)
+    return data
 }
 export default getSurveyMode
