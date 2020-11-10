f4f884ec00b007467893897c8352aac05f6301a3
Swathi Krishnaraja <44269435+SwathiKrishnaraja@users.noreply.github.com>
2020-11-10 22:43:29 +0100

    feat: add translations

--- client/src/locales/en/translationEN.js
+++ client/src/locales/en/translationEN.js
@@ -1,20 +1,363 @@
 export const translationEN = {
   translation: {
     homeContent: {
-      heading: 'Master Thesis – Exercise and Sports',
-      greeting: 'Hello',
+      heading: "Master Thesis – Exercise and Sports",
+      greeting: "Hello",
       content1:
-        'I would like to invite you to take part in a survey about sports and exercise as part of my master thesis. Please answer the questions as honestly as possible. Of course, all your data will be kept anonymous and confidential and cannot be traced back to you. If you are not sure which answer applies to you, please choose the alternative that applies to you.',
-      content2: 'It will take about 10-15 minutes to answer the questionnaire.',
-      content3: 'Thank you for your cooperation,',
-      name: 'Swathi Krishnaraja',
-      button: 'Continue',
+        "I would like to invite you to take part in a survey about sports and exercise as part of my master thesis. Please answer the questions as honestly as possible. Of course, all your data will be kept anonymous and confidential and cannot be traced back to you. If you are not sure which answer applies to you, please choose the alternative that applies to you.",
+      content2: "It will take about 10-15 minutes to answer the questionnaire.",
+      content3:
+        "Information on data protection can be found in the data protection information. Your feedback will help us to improve the experience - so every feedback is appreciated. Thank you for your cooperation,",
+      name: "Swathi Krishnaraja",
+      button: "Continue",
     },
     badges: {
-      title: 'Your Badges:',
+      title: "Your Badges:",
     },
     gamifiedSurvey: {
-      submit: 'Submit',
+      submit: "Submit",
+    },
+    dataProtectionInformation: {
+      header: "Data Protection Information",
+      title1: "General information",
+      content1:
+        "The following study is a part of a Master thesis research conducted at the German Research Center for Artificial Intelligence (DFKI), Saarland University. Voluntary participation and freely given consent. Your participation in this research is entirely voluntary. The consent to participate in this study can be revoked at any time and without giving reasons until the end of the data collection process by either quitting the survey (a corresponding button is available on the website) or by closing the browser window. After the end of data collection, the statutory rights of revocation, information, correction, blocking and deletion can no longer be exercised because the data can no longer be attributed to the data subject.",
+      title2: "Data collection",
+      content2:
+        "After consent to the collection of data, the following data are collected and processed anonymously: We collect all entries in the online survey, Few entries request personal information such as age, gender, city you live in, educational status, occupational status, and marital status. All these attributes are anonymous and the true identity of the participant is never requested (ie name, email address, etc are not collected). Also, we do not match the IP addresses of users to the survey data. However, in this survey, we include certain open-ended questions (which does not ask for any personal information or data), where you are allowed to freely enter any data/ information. Therefore, if you enter personal data in such fields, we cannot identify them. We securely store this data until the end of this research project. We respect your trust and protect your privacy. Therefore, we will never sell or share this data with any third parties, and the collected data is purely used for scientific and research purposes only.",
+      title3: "Contact person",
+      content3:
+        "This study is conducted as a part of a Master thesis project. Therefore, if you have any questions that has not been answered in this privacy statement, or if you would like further information on data protection, please contact Swathi Krishnaraja (s8swkris@stud.uni-saarland.de) or Dr. Pascal Lessel (pascal.lessel@dfki.de). In case, you need additional information regarding the data protection regulations, contact the data protection officer: datenschutz@dfki.de.",
+      title4: "Right of appeal",
+      content4:
+        "You have the right to complain to a supervisory authority if you are of the opinion that the processing of your personal data violates statutory data protection regulations.",
+    },
+    legalInformation: {
+      header: "Legal Information",
+      title:
+        "Deutsches Forschungszentrum für Künstliche Intelligenz GmbH (DFKI) (German Research Center for Artificial Intelligence)",
+    },
+    preSurvey: {
+      q1:
+        "The following words describe different feelings and sensations. Read each word and then enter the intensity of the feeling you are experiencing in the scale next to each word. You have the possibility to choose between five different levels. ",
+      q1a1: "Active",
+      q1a2: "Upset",
+      q1a3: "Hostile",
+      q1a4: "Inspired",
+      q1a5: "Ashamed",
+      q1a6: "Alert",
+      q1a7: "Nervous",
+      q1a8: "Determined",
+      q1a9: "Attentive",
+      q1a10: "Afraid",
+    },
+    mainSurvey: {
+      q1:
+        "How do you feel in general? Please choose one of the following answers:",
+      q1a1: "Very Healthy",
+      q1a2: "Somewhat Healthy",
+      q1a3: "Somewhat Unhealthy",
+      q1a4: "Very Unhealthy",
+      q1a5: "No Answer",
+
+      q2:
+        "And how would you rate yourself in all things, are you. Please choose one of the following answers:",
+      q2a1: "Very Atheletic",
+      q2a2: "Rather Sporty",
+      q2a3: "Rather not Sporty",
+      q2a4: "Not Atheletic at all",
+      q2a5: "No Answer",
+
+      q3:
+        "And how would you describe yourself? Are you...Please choose one of the following answers:",
+      q3a1: "More sociable and likes to be around people",
+      q3a2: "Rather lonely and likes to be alone",
+      q3a3: "No Answer",
+
+      q4:
+        "Todays survey is about exercise and sport. This refers to ALL the physical activities you do. This can be either in a sports club or training in your free time. But it also includes daily activities in between (cycling to school/work, walking instead of driving,...) and if you do a lot of exercise during your education/job. In this sense, are you at least physically active from time to time?",
+      q4a1: "Yes",
+      q4a2: "No",
+      q4a3: "No Answer",
+
+      q5:
+        "You will find different activities listed here, please indicate if you are physically active at least once in a while.",
+      q5a1: "In my spare time I do sports to train",
+      q5a2:
+        "I am active in my free time, but mostly spontaneously and without a fixed training goal",
+      q5a3:
+        "I am physically active within the scope of school, my training or my job",
+      q5a4: "I am active in one or more sports clubs",
+
+      q6:
+        "How often are you physically active? Please choose one of the following answers:",
+      q6a1: "Several times a week",
+      q6a2: "Several times per month",
+      q6a3: "Approximately once a month",
+      q6a4: "Less than once a month",
+      q6a5: "No Answer",
+
+      q7:
+        " And how would you rate your sporting and physical activities? Do you...Please choose one of the following answers:",
+      q7a1: "Enough",
+      q7a2: "Rather too much",
+      q7a3: "Rather too little",
+      q7a4: "No Answer",
+
+      q8:
+        "If you are physically active, where is that? Please select one or more items from the list.",
+      q8a1: "at school or at my training place",
+      q8a2: "in public spaces (streets, parks, green spaces, etc.)",
+      q8a3:
+        "within the framework of services provided by youth organizations or youth centers",
+      q8a4: "elsewhere in nature",
+      q8a5: "in a sports club",
+      q8a6:
+        "in public sports facilities (running tracks, swimming pools, climbing facilities, etc.)",
+      q8a7: "in fitness centers",
+      q8a8: "at home",
+      q8a9: "at my workplace",
+
+      q9:
+        "And when you are physically active, do you prefer to be active together with other people, or do you prefer to be alone? Please choose one of the following answers:",
+      q9a1: "I prefer to be active with other people",
+      q9a2: "I prefer to travel alone",
+      q9a3: "likes both",
+      q9a4: "No Answer",
+
+      q10:
+        "There is a variety of physical activities, from playing football to things like rafting, hiking and dancing. What do you do?",
+
+      q11: "Why are you physically active? ",
+
+      q12:
+        "Here are a few more reasons to be physically active, please indicate whether they apply to you very much, rather already, rather not or not at all.",
+      q12a1: "It gives me a chance to think ",
+      q12a2: "My friends push me to",
+      q12a3: "I like doing things with other people",
+      q12a4: "It is a good way for me to relax ",
+      q12a5: "I like to measure myself with others",
+      q12a6: "It is healthy for me  ",
+      q12a7: "I want to be physically attractive ",
+      q12a8: "I can gain strength for everyday life",
+      q12a9: "I have time for myself",
+      q12a10: "It is fun",
+      q12a11: "I get to know my limits",
+      q12a12: "My parents push me to",
+      q12a13: "I can show what I can do",
+      q12a14: "It helps me relax and relieve stress",
+
+      q13:
+        "Whats your circle of friends like? Are the people in your circle of friends, all things considered... Please choose one of the following answers:",
+      q13a1: "physically more active than you",
+      q13a2: "about as physically active as you",
+      q13a3: "physically less active than you",
+      q13a4: "No Answer",
+
+      q14: "Why are you not physically active?",
+
+      q15:
+        "Here are a few more reasons to be physically active, please indicate whether they apply to you very much, rather already, rather not or not at all.",
+      q15a1: "Sports does more harm than benefits",
+      q15a2: "Sports in unnecessary ",
+      q15a3: "I cannot do sport, due to health or physical reasons",
+      q15a4:
+        "I do not have the opportunity to do so, there is none or too little offers",
+      q15a5: "I do not like the talk of sports and fitness",
+      q15a6: "I cannot motivate myself ",
+      q15a7: "I do not have time for sports ",
+      q15a8: "I do not have anybody to workout or do sport with",
+      q15a9: "I cannot afford it ",
+      q15a10: "I feel uncomfortable when others watch me",
+      q15a11: "I am not fit enough and that frustrates me",
+      q15a12: "I have bad experience",
+
+      q16:
+        "Here you will find again some statements, please indicate in each case, if you want to give them very much agree, rather agree, rather disagree or disagree at all.",
+      q16a1: "Exercise must first and foremost be fun",
+      q16a2: "Non-sporty people are bullied more often ",
+      q16a3: "A daily gym or exercise lesson at school is important",
+      q16a4: "Offers for trendy sports are hard to find ",
+      q16a5: "Opening hours are too short at most sports facilities",
+      q16a6: "Sport is primarily about performance and competition",
+      q16a7: "Sport is taken far too seriously in our society",
+      q16a8: "Exercise is first and foremost for health ",
+      q16a9: "Exercise helps me to get a more beautiful body",
+
+      q17:
+        "Below are a few measures to motivate young people to be more active. Please indicate whether you believe that these young people can be motivated to take more exercise:",
+      q17a1: "More physical activity in school/at work ",
+      q17a2: "TV commercials with short fitness exercises",
+      q17a3: "Fitness stations in public areas",
+      q17a4: "More sports activities in clubs",
+
+      q18:
+        "When you think of your circle of friends now, what would you say? Do most people in your circle of friends wear their outfits when they exercise...",
+      q18a1: "highly relevant",
+      q18a2: "rather important",
+      q18a3: "rather not important",
+      q18a4: "not relevant",
+
+      q19: "Another question: How do you feel with your looks...",
+      q19a1: "very happy",
+      q19a2: "rather content",
+      q19a3: "rather dissatisfied",
+      q19a4: "very unhappy",
+
+      q20: "Finally, we ask you to provide some statistics. How old are you?",
+      q20a1: "Below 18",
+      q20a2: "18 - 24",
+      q20a3: "24 - 34",
+      q20a4: "34 - 44",
+      q20a5: "44 and above",
+
+      q21: "Your Gender:",
+      q21a1: "Male",
+      q21a2: "Female",
+      q21a3: "Non-binary/ Genderqueer",
+      q21a4: "Gender variant/ Non-conforming",
+      q21a5: "Not listed",
+      q21a6: "Prefer not to answer",
+
+      q22: "What state do you live in?",
+
+      q23: "What is your highest completed education?",
+
+      q24: "And you are in which line of work… ",
+
+      q25: "Are you",
+
+      q26: "Do you have children of your own?",
+
+      q27: "You live in:",
+      q27a1: "at your parents house",
+      q27a2: "a seperate apartment or house",
+      q27a3: "in a shared apartment",
+
+      q28: "How big is your place where you live in?",
+      q28a1: "up to 5,000 inhabitants",
+      q28a2: "up to 50,000 inhanitants",
+      q28a3: "over 50,000 inhabitants",
+
+      q29:
+        "Do you play computer games? This also includes games on smartphones, tablets, consoles and handhelds (Nintendo 3Ds, Sony Vita,.).",
+      q29a1: "Yes",
+      q29a2: "No",
+      q29a3: "No answer",
+
+      q30:
+        "Are you familiar with badges (or points and leaderboard) from computer games?",
+      q30a1: "Yes",
+      q30a2: "No",
+      q30a3: "No answer",
+    },
+    postSurvey: {
+      q1:
+        "We would like to know again: How are you feeling right now? The following words describe different feelings and sensations. Read each word and then enter the intensity of that feeling in the scale next to each word. You have the possibility to choose between five different levels.",
+      q1a1: "Active",
+      q1a2: "Upset",
+      q1a3: "Hostile",
+      q1a4: "Inspired",
+      q1a5: "Ashamed",
+      q1a6: "Alert",
+      q1a7: "Nervous",
+      q1a8: "Determined",
+      q1a9: "Attentive",
+      q1a10: "Afraid",
+
+      q2:
+        "Below are some statements about the questionnaire itself. Please indicate in each case whether you agree with them strongly, rather agree, rather disagree or disagree not at all.",
+      q2a1: "The questionnaire was entertaining",
+      q2a2: "Completing the questionnaire took a lot of time",
+      q2a3:
+        "The questionnaire was designed differently from conventional questionnaires",
+      q2a4: "I liked the questionnaire better than conventional questionnaires",
+
+      q3: "What did you like about the survey? ",
+
+      q4: "What did you not like about the survey?",
+
+      q5: "What would you change about the survey?",
+
+      q6:
+        'Below you will find word pairs that you can use to rate the survey. They each represent extreme opposites, between which a gradation is possible. An example: Unsympathetic (insert radio buttons) Sympathetic. This assessment means that the survey is rather sympathetic, but there is still room for improvement. Dont think about the word pairs for a long time, but please give the rating that comes to your mind spontaneously. Some word pairs may not fit the survey so well, but please always tick one answer. Remember that there are no "right" or "wrong" answers - only your personal opinion counts! Please give your impression of the survey using the following word pairs.',
+      q6a1: "",
+
+      q7:
+        "Please give your impression of the survey using the following word pairs.",
+    },
+    selfReportChoiceEnableDisable: {
+      q1:
+        "I liked that I was able to decide whether I want to use game elements or not.",
+      q1a1: "strongly disagree",
+      q1a2: "disagree",
+      q1a3: "neither",
+      q1a4: "agree",
+      q1a5: "strongly agree",
+
+      q2:
+        "I felt that I had control over which survey version I wanted to work with.",
+      q2a1: "strongly disagree",
+      q2a2: "disagree",
+      q2a3: "neither",
+      q2a4: "agree",
+      q2a5: "strongly agree",
+
+      q3:
+        "Overall, what did you feel about the choice given to you to either enable or disable gamification in the survey?",
+    },
+    selfReportChoiceG1G2: {
+      q1:
+        "I liked that I was able to decide which game elements I want to use.",
+      q1a1: "strongly disagree",
+      q1a2: "disagree",
+      q1a3: "neither",
+      q1a4: "agree",
+      q1a5: "strongly agree",
+
+      q2:
+        "I felt that I had control over which survey version I wanted to work with.",
+      q2a1: "strongly disagree",
+      q2a2: "disagree",
+      q2a3: "neither",
+      q2a4: "agree",
+      q2a5: "strongly agree",
+
+      q3: "I would have liked if I could have had more choices to choose from.",
+      q3a1: "strongly disagree",
+      q3a2: "disagree",
+      q3a3: "neither",
+      q3a4: "agree",
+      q3a5: "strongly agree",
+
+      q4:
+        "I would have liked if I could have decided whether I wanted to use gamification or not.",
+      q4a1: "strongly disagree",
+      q4a2: "disagree",
+      q4a3: "neither",
+      q4a4: "agree",
+      q4a5: "strongly agree",
+
+      q5:
+        "Overall, what did you feel about the choice given to you to choose between two gamified versions of the survey?",
+    },
+    selfReportGamification: {
+      q1:
+        "I would have liked if I could have decided whether I wanted to use gamification or not.",
+      q1a1: "strongly disagree",
+      q1a2: "disagree",
+      q1a3: "neither",
+      q1a4: "agree",
+      q1a5: "strongly agree",
+
+      q2: " I would have liked if I decided which game elements I want.",
+      q2a1: "strongly disagree",
+      q2a2: "disagree",
+      q2a3: "neither",
+      q2a4: "agree",
+      q2a5: "strongly agree",
+
+      q3: "Overall, what did you feel about gamification in survey?",
     },
   },
 };
