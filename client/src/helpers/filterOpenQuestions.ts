5edae7271045bf44d7bb34b18a204c7971b03c7d
Swathi Krishnaraja <swathikrishnaraja149@gmail.com>
2020-11-23 20:28:54 +0100

    fix: change open questions list

--- client/src/helpers/filterOpenQuestions.ts
+++ client/src/helpers/filterOpenQuestions.ts
@@ -1,16 +1,16 @@
 import { SurveyQuestionsType } from '../types/types'
 
-const openQuestions = ['q10','q11', 'q14', 'q20','q21','q22','q23','q24' ]
+const openQuestions = ['q10', 'q11', 'q14', 'q20']
 
-const filterOpenQuestions = (listOfQuestions: Array<SurveyQuestionsType>) => 
+const filterOpenQuestions = (listOfQuestions: Array<SurveyQuestionsType>) =>
     listOfQuestions && listOfQuestions
-    .map(question => {
-        return Object.keys(question)
-        .filter(key => openQuestions.includes(key))
-        .reduce((obj:SurveyQuestionsType, key) => {
-            obj[key] = question[key]
-            return obj
-        },{})   
+        .map(question => {
+            return Object.keys(question)
+                .filter(key => openQuestions.includes(key))
+                .reduce((obj: SurveyQuestionsType, key) => {
+                    obj[key] = question[key]
+                    return obj
+                }, {})
         })
 
 
