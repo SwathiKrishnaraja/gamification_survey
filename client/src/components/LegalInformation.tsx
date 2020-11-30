d8ef8d12ab59b9862408db92630af98762badb2b
Swathi Krishnaraja <swathikrishnaraja149@gmail.com>
2020-11-30 15:33:26 +0100

    fix: adjust legal information

--- client/src/components/LegalInformation.tsx
+++ client/src/components/LegalInformation.tsx
@@ -35,58 +35,28 @@ text-align:left;
 const LegalInformation: React.FC = () => {
     return (
         <Container>
-            <Title>Deutsches Forschungszentrum für Künstliche Intelligenz GmbH (DFKI) </Title>
-            <Subtitle> (German Research Center for Artificial Intelligence)</Subtitle>
+            <Title>Saarland University</Title>
+            <Subtitle>Saarland Informatics Campus</Subtitle>
 
             <SubHeading>Postal Address</SubHeading>
-            <AddressLine>Postfach 20 80 </AddressLine>
-            <AddressLine>67608 Kaiserslautern</AddressLine>
-            <AddressLine>Germany</AddressLine>
-            <a style={{ textAlign: "left" }} href="info@dfki.de">info@dfki.de</a>
-
-            <SubHeading>Kaiserslautern Site</SubHeading>
-            <AddressLine>Trippstadter Str. 122 </AddressLine>
-            <AddressLine>67663 Kaiserslautern</AddressLine>
-            <AddressLine>Germany</AddressLine>
-            <AddressLine>Phone: +49 631 20575 0</AddressLine>
-            <AddressLine>Fax: +49 631 20575 5030</AddressLine>
-
-            <SubHeading>Saarbrücken Site</SubHeading>
-            <AddressLine>Campus D3_2 </AddressLine>
-            <AddressLine>Stuhlsatzenhausweg 3</AddressLine>
             <AddressLine>66123 Saarbrücken</AddressLine>
             <AddressLine>Germany</AddressLine>
-            <AddressLine>Phone: +49 681 85775 0</AddressLine>
-            <AddressLine>Fax: +49 681 85775 5341</AddressLine>
-
-            <SubHeading>Bremen Site</SubHeading>
-            <AddressLine>Robert-Hooke-Straße 1</AddressLine>
-            <AddressLine>28359 Bremen</AddressLine>
-            <AddressLine>Germany</AddressLine>
-            <AddressLine>Phone: +49 421 17845 0</AddressLine>
-            <AddressLine>Fax: +49 421 17845 4150</AddressLine>
-
-            <SubHeading>DFKI Project Office Berlin </SubHeading>
-            <AddressLine>Alt-Moabit 91c</AddressLine>
-            <AddressLine>10559 Berlin</AddressLine>
-            <AddressLine>Germany</AddressLine>
-            <AddressLine>Phone: +49 30 23895 0</AddressLine>
-
-            <SubHeading>Management</SubHeading>
-            <AddressLine>Prof. Dr. Antonio Krüger</AddressLine>
-
-            <SubHeading>Head of Supervisory Board</SubHeading>
-            <AddressLine>Dr.-Ing. Gabriël Clemens</AddressLine>
-            <AddressLine>Register Court: Amtsgericht Kaiserslautern</AddressLine>
-            <AddressLine>Register Number: HRB 2313</AddressLine>
+            <a style={{ textAlign: "left" }} href="#">info@uni-saarland.de</a>
 
 
-            <span>
-                <SubHeading>ID-Number:</SubHeading>
-                <AddressLine>DE 148 646 973</AddressLine>
-            </span>
+            <SubHeading>Contact Persons</SubHeading>
+            <SubHeading>Swathi Krishnaraja</SubHeading>
+            <AddressLine>Saarland Informatics Campus</AddressLine>
+            <AddressLine>Stuhlsatzenhausweg 3</AddressLine>
+            <AddressLine>D-66123 Saarbrücken</AddressLine>
+            <AddressLine>Email: <a style={{ textAlign: "left" }} href="#">s8swkris@stud.uni-saarland.de</a></AddressLine>
 
-
+            <SubHeading>Dr.-Ing. Pascal Lessel</SubHeading>
+            <AddressLine>DFKI GmbH</AddressLine>
+            <AddressLine>Saarland Informatics Campus</AddressLine>
+            <AddressLine>Stuhlsatzenhausweg 3</AddressLine>
+            <AddressLine>D-66123 Saarbrücken</AddressLine>
+            <AddressLine>Email: <a style={{ textAlign: "left" }} href="#">Pascal.Lessel@dfki.de</a></AddressLine>
 
 
         </Container>
