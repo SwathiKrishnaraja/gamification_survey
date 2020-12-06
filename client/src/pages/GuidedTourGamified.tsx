0da1d558e9844c2bbe9e5e73716efba4f33914c4
Swathi Krishnaraja <swathikrishnaraja149@gmail.com>
2020-12-06 23:19:20 +0100

    fix: guided tour issues

--- client/src/pages/GuidedTourGamified.tsx
+++ client/src/pages/GuidedTourGamified.tsx
@@ -104,8 +104,12 @@ const GuidedTourGamified = () => {
                 hideBackButton={true}
                 styles={{
                     options: {
-                        arrowColor: '#f5f5f5',
-                        backgroundColor: '#f5f5f5',
+                        arrowColor: '#b1cee9',
+                        backgroundColor: '#b1cee9',
+                        overlayColor: 'rgba(79, 26, 0, 0.4)',
+                        primaryColor: '#000',
+                        textColor: '#004a14',
+                        width: 500,
                         zIndex: 1000,
                     }
                 }}
