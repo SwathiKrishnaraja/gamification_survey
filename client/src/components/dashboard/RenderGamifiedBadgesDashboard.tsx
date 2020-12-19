9e19e96b5199d03cb3b66deac34f54c020d10d33
Swathi Krishnaraja <swathikrishnaraja149@gmail.com>
2020-12-19 19:23:52 +0100

    fix: use unique elements for dashboard

--- client/src/components/dashboard/RenderGamifiedBadgesDashboard.tsx
+++ client/src/components/dashboard/RenderGamifiedBadgesDashboard.tsx
@@ -24,9 +24,14 @@ type RenderGamifiedBadgesDashboardProps = {
     listOfAchievedBadges: Array<Badge>
 }
 const RenderGamifiedBadgesDashboard: React.FC<RenderGamifiedBadgesDashboardProps> = ({ listOfAchievedBadges }) => {
+    const uniqueListOfBadges = [...Array.from(new Set(listOfAchievedBadges.map(element => element.id)))].reduce((result: Array<Badge>, uniqEl) => {
+        result.push(...listOfAchievedBadges.filter(badge => badge.id === uniqEl))
+        return result
+    }, [])
+
     return <Fragment>
         <span style={{ margin: 10 }}>Congratulations ! You have achieved the following badges.</span>
-        {listOfAchievedBadges
+        {uniqueListOfBadges
             .filter(badge => badge.isAchieved)
             .map(badge => (<RenderBadge key={badge.id} src={badge.src} name={badge.name} description={badge.description} />))}
     </Fragment>
