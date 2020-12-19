3b21ca43ec90d70fdd5ccf60eaa691a689dd61cc
Swathi Krishnaraja <swathikrishnaraja149@gmail.com>
2020-12-19 17:58:01 +0100

    style: position leaderboard as sticky

--- client/src/helpers/createLeaderboardElements.tsx
+++ client/src/helpers/createLeaderboardElements.tsx
@@ -4,14 +4,14 @@ const createABarGraphElement = (name: string, points: number, height: number, co
 
     return <div className='leader-element' key={Math.random()}>
         <span>{points} </span>
-        <div style={{ width: 40, height: height, backgroundColor: color, marginLeft: 10 }}> </div>
+        <div style={{ width: 40, height: height, backgroundColor: color, borderRadius: 5 }}> </div>
         <span> {name} </span>
     </div >
 }
-const zeus = createABarGraphElement('zeus', 2200, 220)
-const ron = createABarGraphElement('ron', 800, 80)
-const han = createABarGraphElement('han', 1200, 120)
-const jonas = createABarGraphElement('jonas', 500, 50)
+const zeus = createABarGraphElement('Mark', 2200, 220)
+const ron = createABarGraphElement('Sash', 800, 80)
+const han = createABarGraphElement('Jonas', 1200, 120)
+const jonas = createABarGraphElement('Alex', 500, 50)
 
 export default createABarGraphElement
 
